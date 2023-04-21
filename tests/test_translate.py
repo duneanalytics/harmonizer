4e0402e81aaae69082402692d3a64ab51739d65c
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2023-04-21 12:50:18 +0200

    Add more test cases for hex strings

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -2,18 +2,18 @@ import pytest
 
 from dune.harmonizer import translate_postgres, translate_spark
 from tests.cases import postgres_test_cases, spark_test_cases
-from tests.helpers import assert_output, read_test_case
+from tests.helpers import canonicalize, read_test_case
 
 
 @pytest.mark.parametrize("test_case", postgres_test_cases)
 def test_translate_postgres(test_case):
     query, expected_output = read_test_case(test_case)
     output = translate_postgres(query=query, dataset=test_case.dataset)
-    assert_output(output, expected_output)
+    assert expected_output == canonicalize(output)
 
 
 @pytest.mark.parametrize("test_case", spark_test_cases)
 def test_translate_spark(test_case):
     query, expected_output = read_test_case(test_case)
     output = translate_spark(query=query)
-    assert_output(output, expected_output)
+    assert expected_output == canonicalize(output)
