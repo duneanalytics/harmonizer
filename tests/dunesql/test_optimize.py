920ccb67ccf38a0d79f31946e0a3cea30930be12
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2023-05-11 13:06:35 +0200

    Rename a test

--- tests/dunesql/test_optimize.py
+++ tests/dunesql/test_optimize.py
@@ -4,7 +4,7 @@ from dune.harmonizer.dunesql import DuneSQL
 from dune.harmonizer.dunesql.optimize import optimize
 
 
-def test_with_schema():
+def test_optimize_cast_with_schema():
     dune_sql_expr = sqlglot.parse_one("SELECT col = 1 FROM tbl", read=DuneSQL)
     optimized = optimize(dune_sql_expr, schema={"tbl": {"col": "double"}})
     assert "SELECT tbl.col = CAST(1 AS DOUBLE) AS _col_0 FROM tbl" == optimized.sql(DuneSQL)
