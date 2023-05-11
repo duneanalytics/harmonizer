4805de061868864e52a4b6f658b870edf84f2564
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2023-05-11 13:27:56 +0200

    Add test where optimize fails due to unknown column

--- tests/dunesql/test_optimize.py
+++ tests/dunesql/test_optimize.py
@@ -1,3 +1,4 @@
+import pytest
 import sqlglot
 
 from dune.harmonizer.dunesql.dunesql import DuneSQL
@@ -8,3 +9,9 @@ def test_optimize_cast_with_schema():
     dune_sql_expr = sqlglot.parse_one("SELECT col = 1 FROM tbl", read=DuneSQL)
     optimized = optimize(dune_sql_expr, schema={"tbl": {"col": "double"}})
     assert "SELECT tbl.col = CAST(1 AS DOUBLE) AS _col_0 FROM tbl" == optimized.sql(DuneSQL)
+
+
+def test_optimize_fails():
+    dune_sql_expr = sqlglot.parse_one("SELECT col = 1 FROM tbl", read=DuneSQL)
+    with pytest.raises(sqlglot.errors.OptimizeError, match="Unknown column: col"):
+        optimize(dune_sql_expr, schema={"tbl": {"x": "int"}})
