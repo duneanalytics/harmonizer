124119270ad669045fd1b3c731332c54a2f9eb7b
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2023-05-11 14:11:50 +0200

    Run black

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -9,12 +9,15 @@ from dune.harmonizer.schemas import schema_from_sqlite
 
 @pytest.fixture
 def schema_db():
-    path = (Path(__file__).parent / "tmp.db")
+    path = Path(__file__).parent / "tmp.db"
     table_name = "schemas"
     conn = sqlite3.connect(str(path))
     with closing(conn.cursor()) as cursor:
         cursor.execute(f"CREATE TABLE {table_name} (table_name, column_name, sqlglot_type)")
-        cursor.executemany(f"INSERT INTO {table_name} VALUES(?, ?, ?)", [("tbl", "col", "int"), ("tbl", "x", "boolean"), ("other_tbl", "z", "text")])
+        cursor.executemany(
+            f"INSERT INTO {table_name} VALUES(?, ?, ?)",
+            [("tbl", "col", "int"), ("tbl", "x", "boolean"), ("other_tbl", "z", "text")],
+        )
         conn.commit()
     yield path, table_name
     path.unlink()  # delete database
