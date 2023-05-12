duneanalytics/harmonizer

commit 9172aded6ddf6c48a1a659f3bf198483874ee97a

Add alias to simple casts if query is Postgres (#59)
In Postgres, `cast(x) as type` and `x::type` has alias `x`, whereas in
Trino it does not. We should support this simple case, as some users
have come to inadvertently rely on it. It's not the case in Spark and
it's not the case for non-column expressions. Since it's only the case
in Postgres, we need to add it outside the DuneSQL dialect.

Fixes #55

dune/harmonizer/custom_transforms.py
@@ -272,6 +272,15 @@ def chain_where(dataset):
     }[dataset]
 
 
+def explicit_alias_on_cast(query_tree):
+    """In Postgres, a simple cast of a column will retain the column name, so we add an explicit cast"""
+    return query_tree.transform(
+        lambda e: sqlglot.exp.Alias(this=e, alias=e.alias_or_name)
+        if isinstance(e, sqlglot.exp.Cast) and isinstance(e.this, sqlglot.exp.Column)
+        else e
+    )
+
+
 def postgres_transforms(query):
     """Apply a series of transforms to the query tree, recursively using SQLGlot's recursive transform function.
 
@@ -282,6 +291,7 @@ def postgres_transforms(query):
         cast_timestamp_parameters,
         warn_sequence,
         bytearray_parameter_fix,
+        explicit_alias_on_cast,
     )
     for f in transforms:
         query_tree = query_tree.transform(f)

tests/test_cases/postgres/dex_ethereum.out
@@ -2,7 +2,7 @@
 
 SELECT
   project AS "Project",
-  SUM(CAST(amount_usd AS DOUBLE)) AS usd_volume
+  SUM(CAST(amount_usd AS DOUBLE) AS amount_usd) AS usd_volume
 FROM (
   SELECT
     *

tests/test_cases/postgres/dex_polygon.out
@@ -2,7 +2,7 @@
 
 SELECT
   project AS "Project",
-  SUM(CAST(amount_usd AS DOUBLE)) AS usd_volume
+  SUM(CAST(amount_usd AS DOUBLE) AS amount_usd) AS usd_volume
 FROM (
   SELECT
     *

tests/test_cases/postgres/explicit_cast.in
@@ -1,0 +1,1 @@
+select cast(x as int), y::text, cast(c + 1 as int) from t

tests/test_cases/postgres/explicit_cast.out
@@ -1,0 +1,7 @@
+/* Success! If you're still running into issues, check out https://dune.com/docs/query/syntax-differences/ or reach out in the #dune-sql Discord channel. */
+
+SELECT
+  TRY_CAST(x AS INTEGER) AS x,
+  TRY_CAST(y AS VARCHAR) AS y,
+  TRY_CAST(c + 1 AS INTEGER)
+FROM t

tests/cases.py
@@ -24,6 +24,7 @@ postgres_test_cases = [
     ),
     PostgresTestCase("test_cases/postgres/cross_join.in", "test_cases/postgres/cross_join.out", "ethereum"),
     PostgresTestCase("test_cases/postgres/median.in", "test_cases/postgres/median.out", "ethereum"),
+    PostgresTestCase("test_cases/postgres/explicit_cast.in", "test_cases/postgres/explicit_cast.out", "ethereum"),
 ]
 
 

commit 35afa526e25c5327dcdbdb7d9f35df76252ac537

Only add an alias to simple casts if the expression is a root expression in the select (#61)
We got a regression after #59, where _any_ simple cast of a column got
an alias, even if it was inside another expression. This fixes that
regression so we only add an alias if the expression is a root
expression in the select.

dune/harmonizer/custom_transforms.py
@@ -276,7 +276,9 @@ def explicit_alias_on_cast(query_tree):
     """In Postgres, a simple cast of a column will retain the column name, so we add an explicit cast"""
     return query_tree.transform(
         lambda e: sqlglot.exp.Alias(this=e, alias=e.alias_or_name)
-        if isinstance(e, sqlglot.exp.Cast) and isinstance(e.this, sqlglot.exp.Column)
+        if isinstance(e, sqlglot.exp.Cast)
+        and isinstance(e.this, sqlglot.exp.Column)
+        and isinstance(e.parent, sqlglot.exp.Select)
         else e
     )
 

tests/test_cases/postgres/dex_ethereum.out
@@ -2,7 +2,7 @@
 
 SELECT
   project AS "Project",
-  SUM(CAST(amount_usd AS DOUBLE) AS amount_usd) AS usd_volume
+  SUM(CAST(amount_usd AS DOUBLE)) AS usd_volume
 FROM (
   SELECT
     *

tests/test_cases/postgres/dex_polygon.out
@@ -2,7 +2,7 @@
 
 SELECT
   project AS "Project",
-  SUM(CAST(amount_usd AS DOUBLE) AS amount_usd) AS usd_volume
+  SUM(CAST(amount_usd AS DOUBLE)) AS usd_volume
 FROM (
   SELECT
     *

tests/test_cases/postgres/explicit_cast.in
@@ -1,1 +1,1 @@
-select cast(x as int), y::text, cast(c + 1 as int) from t
+select cast(x as int), y::text, cast(c + 1 as int), 1 + cast(d as int), cast(z as text) as foo from t

tests/test_cases/postgres/explicit_cast.out
@@ -3,5 +3,7 @@
 SELECT
   TRY_CAST(x AS INTEGER) AS x,
   TRY_CAST(y AS VARCHAR) AS y,
-  TRY_CAST(c + 1 AS INTEGER)
+  TRY_CAST(c + 1 AS INTEGER),
+  1 + TRY_CAST(d AS INTEGER),
+  TRY_CAST(z AS VARCHAR) AS foo
 FROM t