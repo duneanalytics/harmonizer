78129c06d771a1e191bcef1820fb47f2c83edf20
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2023-05-15 11:59:01 +0200

    Use replace_tables

--- dune/harmonizer/table_replacements.py
+++ dune/harmonizer/table_replacements.py
@@ -1,6 +1,6 @@
 import sqlglot
 from sqlglot import to_identifier
-from sqlglot.expressions import TableAlias
+from sqlglot.expressions import TableAlias, replace_tables
 
 
 def postgres_table_replacements(dataset):
@@ -13,25 +13,18 @@ def postgres_table_replacements(dataset):
         if not isinstance(table_node, sqlglot.exp.Table):
             return table_node
 
-        spellbook_mapping = {
-            ("erc20", "erc20_evt_transfer"): (f"erc20_{dataset}", "evt_Transfer"),
-            ("bep20", "bep20_evt_transfer"): ("erc20_bnb", "evt_Transfer"),
-            ("erc721", "erc721_evt_transfer"): ("erc721_ethereum", "evt_Transfer"),
-            ("bep20", "tokens"): ("tokens", "erc20"),
-            ("erc20", "tokens"): ("tokens", "erc20"),
-            ("erc721", "tokens"): ("tokens", "nft"),
-            ("prices", "layer1_usd_btc"): ("prices", "usd"),
-            ("prices", "layer1_usd_eth"): ("prices", "usd"),
+        mapping = {
+            "erc20.erc20_evt_transfer": f"erc20_{dataset}.evt_Transfer",
+            "bep20.bep20_evt_transfer": "erc20_bnb.evt_Transfer",
+            "erc721.erc721_evt_transfer": "erc721_ethereum.evt_Transfer",
+            "bep20.tokens": "tokens.erc20",
+            "erc20.tokens": "tokens.erc20",
+            "erc721.tokens": "tokens.nft",
+            "prices.layer1_usd_btc": "prices.usd",
+            "prices.layer1_usd_eth": "prices.usd",
         }
-        table = table_node.db.lower(), table_node.name.lower()
-        replacement = spellbook_mapping.get(table)
-        if replacement is not None:
-            to_db, to_table = replacement
-            return sqlglot.exp.Table(
-                this=to_identifier(to_table),
-                db=to_identifier(to_db),
-                alias=TableAlias(this=to_identifier(table_node.alias)) if table_node.alias else None,
-            )
+
+        table_node = replace_tables(table_node, mapping)
 
         # if decoded table, add _{dataset} to the table name
         if any(decoded in table_node.name.lower() for decoded in ("_evt_", "_call_")):
