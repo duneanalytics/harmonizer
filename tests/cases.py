dc628db436a705f930ae1d9fb1d357d20573e6fd
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2023-06-06 12:23:10 +0200

    Add failing test case

--- tests/cases.py
+++ tests/cases.py
@@ -52,6 +52,7 @@ spark_test_cases = [
     SparkTestCase("test_cases/spark/array_index.in", "test_cases/spark/array_index.out"),
     SparkTestCase("test_cases/spark/0x_strings_leading_0.in", "test_cases/spark/0x_strings_leading_0.out"),
     SparkTestCase("test_cases/spark/datediff.in", "test_cases/spark/datediff.out"),
+SparkTestCase("test_cases/spark/integer_division.in", "test_cases/spark/integer_division.out"),
 ]
 
 
